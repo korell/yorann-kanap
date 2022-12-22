cdc91ea240010b421bbc6f2feb2d872c8f8edee2
Matthieu <hello@matthieurebillard.fr>
2022-12-22 23:00:09 +0100

    [panier] Validation des données du form et refacto

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,17 +5,22 @@ const totalPriceDOM = document.querySelector('#totalPrice')
 const totalQuantityDOM = document.querySelector('#totalQuantity')
 const form = document.querySelector('form')
 
-let cartFullProducts
-let apiProductsData
+// Il est préférable d'écouter la soumission du formulaire plutôt que le clic sur un bouton
+form.addEventListener('submit', onSubmitForm)
+
+// Les données stockées du panier
 let cart = getCartProducts()
+
+// Les données complètes des produits du panier depuis l'API
+// Pas besoin de récupérer les données de tous le produits du catalogue
+// Normalement, une API bien faite permettrait de faire une requête
+// avec une liste d'ids de produits pour récupérer seulement une partie du catalogue
+let apiProductsData  = await getProductsDataFromApi(cart)
 
 // On lance tout le traitement des données s'il y a qq'chose dans le panier
 if(cart) {
-    const productsData = await getProductsDataFromApi(cart)
-    apiProductsData = productsData
-    cartFullProducts = getCartProductsFullData(cart, productsData)
     itemsDOM.innerHTML = ''
-    cartFullProducts.forEach(cartProduct => {
+    cart.forEach(cartProduct => {
         const cartLineElement = buildProductLine(cartProduct)
         itemsDOM.append(cartLineElement)
     })
@@ -38,8 +43,6 @@ if(cart) {
     itemsDOM.innerHTML = '<div>Panier vide</div>'
 }
 
-form.addEventListener('submit', onSubmitForm)
-
 /**
  * Récupération des données des produits du panier
  * S'il y a dans le panier plusieurs fois le même produit,
@@ -49,6 +52,9 @@ form.addEventListener('submit', onSubmitForm)
  * @returns {Promise<Awaited<any>[]>}
  */
 async function getProductsDataFromApi(cart) {
+    if(!cart) {
+        return []
+    }
     const productsCartIds = cart.map(cart => cart.productId)
 
     // Set est une manière de dédupliquer des valeurs d'un tableau
@@ -78,24 +84,17 @@ async function getOneProductDataFromApi(productId) {
 }
 
 /**
- * Récupération des données complètes des produits du panier
- * @param cart
- * @param productsData
- * @returns {*[]}
+ * Retourne toutes les données d'un produit du panier
+ * Données produit + couleur + quantité
+ * @param product
+ * @returns {unknown}
  */
-function getCartProductsFullData(cart, productsData) {
-    const products = []
-    if(cart.length) {
-        cart.forEach(cartItem => {
-            const fullProduct = productsData.find(productData => productData._id === cartItem.productId)
-            // Ici on complète le tableau avec les données du panier et des produits
-            products.push({
-                ...fullProduct,
-                ...cartItem
-            })
-        })
+function getProductFullData(product) {
+    const fullProduct = apiProductsData.find(productData => productData._id === product.productId)
+    return {
+        ...fullProduct,
+        ...product
     }
-    return products
 }
 
 /**
@@ -112,25 +111,26 @@ function getCartProducts() {
  * @returns {HTMLElement}
  */
 function buildProductLine(product) {
+    const productData = getProductFullData(product)
     const article = document.createElement('article')
     article.classList.add('cart__item')
-    article.dataset.id = product.productId
-    article.dataset.color = product.color
+    article.dataset.id = productData.productId
+    article.dataset.color = productData.color
 
     const innerArticleHTML = `
         <div class="cart__item__img">
-            <img src="${product.imagesUrls.medium}" alt="${product.altTxt}">
+            <img src="${productData.imagesUrls.medium}" alt="${productData.altTxt}">
         </div>
         <div class="cart__item__content">
             <div class="cart__item__content__description">
-                <h2>${product.name}</h2>
-                <p>${product.color}</p>
-                <p>${product.price.toLocaleString(undefined, {minimumFractionDigits: 2})} €</p>
+                <h2>${productData.name}</h2>
+                <p>${productData.color}</p>
+                <p>${productData.price.toLocaleString(undefined, {minimumFractionDigits: 2})} €</p>
             </div>
             <div class="cart__item__content__settings">
                 <div class="cart__item__content__settings__quantity">
                     <p>Qté : </p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productData.quantity}">
                 </div>
                 <div class="cart__item__content__settings__delete">
                     <button type="button" class="deleteItem">Supprimer</button>
@@ -138,9 +138,7 @@ function buildProductLine(product) {
             </div>
         </div>
     `
-
     article.innerHTML = innerArticleHTML
-
     return article
 }
 
@@ -162,8 +160,9 @@ function setCartToLocalStorage(cart) {
 function setTotalPrice() {
 
     // La méthode .reduce() est très utile pour faire des sommes
-    const total = cartFullProducts.reduce((acc, product) => {
-        return acc + product.quantity * product.price
+    const total = cart.reduce((acc, product) => {
+        const productData = getProductFullData(product)
+        return acc + productData.quantity * productData.price
     }, 0)
     totalPriceDOM.textContent = total.toLocaleString(
         undefined, {
@@ -175,7 +174,7 @@ function setTotalPrice() {
  * Mise à jour de la quantité totale dans le DOM
  */
 function setTotalQuantity() {
-    const total = cartFullProducts.reduce((acc, product) => {
+    const total = cart.reduce((acc, product) => {
         return acc + parseInt(product.quantity)
     }, 0)
     totalQuantityDOM.textContent = total
@@ -190,12 +189,8 @@ function onChangeItemQuantity(ev) {
     const productId = article.dataset.id
     const productColor = article.dataset.color
     const newQuantity = ev.target.value
-    cartFullProducts = cartFullProducts.map(cartFullProduct => {
-        if(cartFullProduct.productId === productId && cartFullProduct.color === productColor) {
-            cartFullProduct.quantity = parseInt(newQuantity)
-        }
-        return cartFullProduct
-    })
+
+    // Mise à jour du panier avec la nouvelle quantité
     cart = cart.map(cartItem => {
         if(cartItem.productId === productId && cartItem.color === productColor) {
             cartItem.quantity = parseInt(newQuantity)
@@ -215,16 +210,11 @@ function onClickDeleteButton(ev) {
     const article = ev.target.closest('[data-id]')
     const productId = article.dataset.id
     const productColor = article.dataset.color
-    // Suppression du DOM
+    // Suppression de la ligne du DOM
     article.remove()
 
     // Mise à jour de l'objet cart en enlevant le produit supprimé
     cart = cart.filter(cartItem => {
-        return !(productId === cartItem.productId && productColor === cartItem.color)
-    })
-
-    // Mise à jour de notre objet contenant toutes les données
-    cartFullProducts = cartFullProducts.filter(cartItem => {
         return !(productId === cartItem.productId && productColor === cartItem.color)
     })
 
@@ -236,21 +226,119 @@ function onClickDeleteButton(ev) {
 }
 
 /**
+ * Toutes les infos concernant les champs du form sont ici
+ * C'est pratique d'avoir toit au même endroit
+ * @returns {{firstName: {messageErrorContainerId: string, errorsMessages: {format: string, required: string}, format: RegExp}, lastName: {messageErrorContainerId: string, errorsMessages: {format: string, required: string}, format: RegExp}, address: {messageErrorContainerId: string, errorsMessages: {format: string, required: string}, format: RegExp}, city: {messageErrorContainerId: string, errorsMessages: {format: string, required: string}, format: RegExp}, email: {messageErrorContainerId: string, errorsMessages: {format: string, required: string}, format: RegExp}}}
+ */
+function getFieldsDefinitions() {
+    return {
+        firstName: {
+            format: /[\p{Letter}\p{Separator}\p{Dash_Punctuation}]{3,50}/u,
+            messageErrorContainerId: 'firstNameErrorMsg',
+            errorsMessages: {
+                required: 'Prénom requis',
+                format: 'Le prénom doit être compris entre 3 et 50 caractères et ne peut pas contenir de chiffres'
+            }
+        },
+        lastName: {
+            format: /[\p{Letter}\p{Separator}\p{Dash_Punctuation}]{3,50}/u,
+            messageErrorContainerId: 'lastNameErrorMsg',
+            errorsMessages: {
+                required: 'Nom requis',
+                format: 'Le prénom doit être compris entre 3 et 50 caractères et ne peut pas contenir de chiffres'
+            }
+        },
+        address: {
+            format: /^[\p{Letter}\p{Separator}\p{Punctuation}\d]{3,100}$/u,
+            messageErrorContainerId: 'addressErrorMsg',
+            errorsMessages: {
+                required: 'Adresse requise',
+                format: 'L\'adresse doit être comprise entre 3 et 100 caractères alphanumérique'
+            }
+        },
+        city: {
+            format: /^[\w\s,.-]{3,100}$/,
+            messageErrorContainerId: 'cityErrorMsg',
+            errorsMessages: {
+                required: 'Ville requise',
+                format: 'La ville doit être comprise entre 3 et 100 caractères alphanumérique'
+            }
+        },
+        email: {
+            format: /^[0-9a-z_\-.]{1,100}@[0-9a-z\-_.]{1,50}.[a-z]{2,5}$/,
+            messageErrorContainerId: 'emailErrorMsg',
+            errorsMessages: {
+                required: 'Email requis',
+                format: 'Adresse email invalide'
+            }
+        },
+    }
+}
+
+/**
+ * Validation du formulaire
+ * Toute la logique se fait ici
+ * On retourne un booléen success que l'on peut tester ensuite
+ * Si pas d'erreurs, on renvoie les données du form
+ * @param form
+ * @returns {{success: boolean}}
+ */
+function formValidate(form) {
+    const response = {
+        success: true
+    }
+    const userValues = Object.fromEntries(new FormData(form))
+    const fieldsDefinitions = getFieldsDefinitions()
+    //On vérifie si tous les champs sont bien présents
+    const isAllFieldsExists = Object.keys(fieldsDefinitions).every(key => {
+        return Object.keys(userValues).includes(key)
+    })
+    if(!isAllFieldsExists) {
+        console.log('champs manquants');
+    }
+    for (const userValueKey in userValues) {
+        if(!fieldsDefinitions[userValueKey]) {
+            continue
+        }
+        const fieldValue = userValues[userValueKey]
+        const errorContainer = form.querySelector('#' + fieldsDefinitions[userValueKey].messageErrorContainerId)
+        // Champ vide
+        if(fieldValue === '' && fieldsDefinitions[userValueKey]) {
+            response.success = false
+            errorContainer.textContent = fieldsDefinitions[userValueKey].errorsMessages.required
+        }
+        // Mauvais format
+        else if(!fieldsDefinitions[userValueKey].format.test(fieldValue)) {
+            errorContainer.textContent = fieldsDefinitions[userValueKey].errorsMessages.format
+        }
+        // Enfin, si pas d'erreur, on supprime le contenu des erreurs et on envoie les données
+        else {
+            errorContainer.textContent = ''
+            response.formData = userValues
+        }
+    }
+    return response
+}
+
+/**
  * À la soumission du formulaire
  * @param ev
  * @returns {Promise<void>}
  */
 async function onSubmitForm(ev) {
     ev.preventDefault()
-    const contact = Object.fromEntries(new FormData(form))
+    const validation = formValidate(form)
+    if(!validation.success) {
+        return
+    }
     const response = await fetch(API_URL + '/order', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            contact: contact,
-            products: cartFullProducts.map(product => product._id)
+            contact: validation.formData,
+            products: cart.map(product => product.productId)
         })
     }).catch(error => {
         console.log('error', error);
@@ -260,7 +348,6 @@ async function onSubmitForm(ev) {
         const result = await response.json()
         location.href = './confirmation.html?orderId=' + result.orderId
         // suppression des produits du panier
-        cart = []
         setCartToLocalStorage(cart)
     } else {
         const json = await response.json()
